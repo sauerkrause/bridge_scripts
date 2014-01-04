5e3a5d3b0a14cc73e5d686442a20e03c2e2a18a9
Robert Allen Krause <robert.allen.krause@gmail.com>
2014-01-03 22:39:24 -0600

    - Added a starbound producer for redis pubsub.

---
+++ starbound.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+sub match_connection {
+    my ($line) = @_;
+    if($line =~ / \'(.*)\'/) {
+	my $name = $1;
+	if($line =~ /isconnect/) {
+	    return "$name disconnected";
+	} else {
+	    return "$name connected";
+	}
+    } else {
+	return "";
+    }
+}
+
+while( <STDIN> ) {
+    my $line=$_;
+    chomp($line);
+    $ret="";
+    if($line =~ /Info: Client|Info:  </) {
+	if($out = match_connection($line)) {
+	    $ret=$out;
+	} else {
+	    $line =~ s/Info:  //;
+	    $ret="$line";
+	}
+    }
+    chomp($ret);
+    if($ret) {
+	system("redis-cli publish starbound \"$ret\"");
+    }
+}
+
